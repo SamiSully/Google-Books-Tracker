507c4976869a5bcb08520b2a61590c54d6791500
samis <samilsully@gmail.com>
2020-11-07 18:17:37 -0800

    trying again

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const UserController = require("./controllers/userController");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-mern", {
   useNewUrlParser: true,
